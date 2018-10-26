7552c6f4fcec4c069491f1b6dc67000b94ac74e0
xvwvx <339349330@qq.com>
2018-10-26 17:20:26 +0800

    设配新iPhone手机

--- WRNavigationBar/WRCustomNavigationBar.m
+++ WRNavigationBar/WRCustomNavigationBar.m
@@ -268,16 +268,8 @@
     return 44 + CGRectGetHeight([UIApplication sharedApplication].statusBarFrame);
 }
 + (BOOL)isIphoneX {
-    struct utsname systemInfo;
-    uname(&systemInfo);
-    NSString *platform = [NSString stringWithCString:systemInfo.machine encoding:NSASCIIStringEncoding];
-    if ([platform isEqualToString:@"i386"] || [platform isEqualToString:@"x86_64"]) {
-        // judgment by height when in simulators
-        return (CGSizeEqualToSize([UIScreen mainScreen].bounds.size, CGSizeMake(375, 812)) ||
-                CGSizeEqualToSize([UIScreen mainScreen].bounds.size, CGSizeMake(812, 375)));
-    }
-    BOOL isIPhoneX = [platform isEqualToString:@"iPhone10,3"] || [platform isEqualToString:@"iPhone10,6"];
-    return isIPhoneX;
+    NSInteger height = UIScreen.mainScreen.nativeBounds.size.height;
+    return height == 1792 || height == 2436 || height == 2688;
 }
 
 @end
